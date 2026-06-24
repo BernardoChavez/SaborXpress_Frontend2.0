17e94b94da53170423d3035b5e1d8a778716990a
BernardoChavez <bernardochavez595@gmail.com>
2026-06-24 07:42:52 -0400

    Change axios default to /api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../core/store/useAuthStore';
 
-const BASE_URL = import.meta.env.VITE_API_URL ?? 'https://reminder-garment-mangy.ngrok-free.dev/api';
+const BASE_URL = import.meta.env.VITE_API_URL ?? '/api';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
